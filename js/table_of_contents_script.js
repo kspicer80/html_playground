531803ffaab783ba66ae57fb0d5b23121bb0f009
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2023-04-09 07:12:35 -0500

    Update table_of_contents_script.js

--- js/table_of_contents_script.js
+++ js/table_of_contents_script.js
@@ -1,13 +1,12 @@
-<script>
-    var toc = document.getElementById('table-of-contents');
-    var headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+var toc = document.getElementById('table-of-contents');
+var headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-    headings.forEach(function(heading) {
-      var tocLink = document.createElement('a');
-      tocLink.textContent = heading.textContent;
-      tocLink.setAttribute('href', '#' + heading.id);
-      toc.appendChild(tocLink);
+headings.forEach(function(heading) {
+    var tocLink = document.createElement('a');
+    tocLink.textContent = heading.textContent;
+    tocLink.setAttribute('href', '#' + heading.id);
+    toc.appendChild(tocLink);
 
-      toc.appendChild(document.createElement('br')); // Add line break
-    });
-</script>
+    toc.appendChild(document.createElement('br')); // Add line break
+});
+
